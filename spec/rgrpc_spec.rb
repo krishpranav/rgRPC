7049035a722e89d349b3814dd0b962c67f284953
krishpranav <krisna.pranav@gmail.com>
2023-07-20 19:01:04 +0530

    spec: rgrpc_spec

---
+++ spec/rgrpc_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RGrpc do 
+    it 'has a version number' do
+        expect(RGrpc::VERSION).not_to be nil
+    end
+
+    it 'does something useful' do
+        expect(false).to eq(true)
+    end
+end
