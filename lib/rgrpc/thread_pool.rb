ad412662cef78b07011ec26d133cb7eab1a63950
krishpranav <krisna.pranav@gmail.com>
2023-07-20 19:50:50 +0530

    rgrpc: thread_pool

--- lib/rgrpc/thread_pool.rb
+++ lib/rgrpc/thread_pool.rb
@@ -13,21 +13,87 @@ module RGrpc
       @shutdown = false
       @tasks = SizedQueue.new(QUEUE_SIZE)
 
-      @spanwed = 0
+      @spawned = 0
       @workers = []
       @mutex = Mutex.new
       @waiting = 0
 
-      @min_pool_size.times { span_thread }
-      @auto_trimmer = GrpcKit::RpcDispatcher::AutoTrimmer.new(self, interval: interval)
+      @min_pool_size.times { spawn_thread }
+      @auto_trimmer = GrpcKit::RpcDispatcher::AutoTrimmer.new(self, interval: interval + rand(10)).tap(&:start!)
     end
 
     def schedule(task, &block)
-      return if task.nil?
+      if task.nil?
+        return
+      end
 
-      raise "scheduling new task isnn't alloowed during, shutdown!" if @shutdown
+      if @shutdown
+        raise "scheduling new task isn't allowed during shutdown"
+      end
 
       @tasks.push(block || task)
+
+      if @mutex.synchronize { (@waiting < @tasks.size) && (@spawned < @max_pool_size) }
+        spawn_thread
+      end
+    end
+
+    def resouce_available?
+      (@waiting != 0) || (@spawned != @max_pool_size)
+    end
+
+    def shutdown
+      @shutdown = true
+      @max_pool_size.times { @tasks.push(nil) }
+      @auto_trimmer.stop
+      until @workers.empty?
+        RGrpc.logger.debug("Shutdown waiting #{@waiting} workers")
+        sleep 1
+      end
+    end
+
+    def trim(force = false)
+      if @mutex.synchronize { (force || (@waiting > 0)) && (@spawned > @min_pool_size) }
+        GrpcKit.logger.info("Trim worker! Next worker size #{@spawned - 1}")
+        @tasks.push(nil)
+      end
+    end
+
+    private
+
+    def spawn_thread
+      @spawned += 1
+      worker = Thread.new(@spawned) do |i|
+        Thread.current.name = "RGrpc worker thread #{i}"
+        RGrpc.logger.debug("#{Thread.current.name} started")
+
+        loop do
+          if @shutdown
+            break
+          end
+
+          @mutex.synchronize { @waiting += 1 }
+          task = @tasks.pop
+          @mutex.synchronize { @waiting -= 1 }
+          if task.nil?
+            break
+          end
+
+          begin
+            @block.call(task)
+          rescue Exception => e 
+            RGrpc.logger.error("An error occured on top level in worker #{Thread.current.name}: #{e.message} (#{e.class})\n #{Thread.current.backtrace.join("\n")}  ")
+          end
+        end
+
+        RGrpc.logger.debug("worker thread #{Thread.current.name} is stopping")
+        @mutex.synchronize do
+          @spawned -= 1
+          @workers.delete(worker)
+        end
+      end
+
+      @workers.push(worker)
     end
   end
 end
