d93c7f6d9d0198e8894bbf523107987adf45216b
krishpranav <krisna.pranav@gmail.com>
2023-07-21 19:32:53 +0530

    rgrpc: engine[single]

---
+++ lib/rgrpc/engine/single.rb
@@ -0,0 +1,49 @@
+require 'rgprc/server'
+
+module RGrpc
+  module Engine
+    class Single
+      def self.create(config)
+        serv = RGrpc::Server.new(
+          min_pool_size: config[:min_pool_size],
+          max_pool_size: config[:max_pool_size],
+          min_connection_size: config[:min_connection_size],
+          max_connection_size: config[:max_connection_size],
+          max_receive_message_size: config[:max_receive_message_size],
+          max_send_message_size: config[:max_send_message_size],
+          interceptors: config[:interceptors]
+        )
+        new(serv, config)
+      end
+
+      def initialize(server, config)
+        @server = server
+        @config = config
+        @sock = TCPServer.new(@config[:bind], @config[:port])
+        @sock.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+      end
+
+      def run
+        @config[:services].each do |s|
+          @server.handle(s)
+        end
+
+        install_handler
+
+        @server.before_run
+        @sock.listen(Socket::SOMAXCONN)
+        @server.run(@sock)
+      end
+
+      def install_handler
+        trap('INT') do
+          @server.shutdown
+        end
+
+        trap('TERM') do
+          @server.shutdown
+        end
+      end
+    end
+  end
+end
