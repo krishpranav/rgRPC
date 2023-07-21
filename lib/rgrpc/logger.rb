7b1503fc4d1dda560f438b9dc3506e0e76288751
krishpranav <krisna.pranav@gmail.com>
2023-07-21 19:24:08 +0530

    rgrpc: logger

--- lib/rgrpc/logger.rb
+++ lib/rgrpc/logger.rb
@@ -1,34 +1,33 @@
 module RGrpc
     class Logger
-        class << self
-            def setup(config)
-                config[:logger] = RGrpc::Logger.create(config)
-                RGrpc.logger = config[:logger]
-
-                m = Module.new do
-                    def logger
-                        RGrpc.logger
-                    end
-                end
-
-                GrpcKit::Grpc.extend(m)
-
-                def create(config)
-                    config[:logger] || ServerEngine::DaemonLogger.new(config)
-                end
-
-                def logdev_from_config(config)
-                    case c = config[:log]
-                    when nil
-                        STDERR
-                    when '-'
-                        STDOUT
-                    else
-                        c
-                    end
-                end
+      class << self
+        def setup(config)
+          config[:logger] = RGrpc::Logger.create(config)
+          RGrpc.logger = config[:logger]
+  
+          m = Module.new do
+            def logger
+              RGrpc.logger
             end
-        end
+          end
 
+          GrpcKit::Grpc.extend(m)
+        end
+  
+        def create(config)
+          config[:logger] || ServerEngine::DaemonLogger.new(logdev_from_config(config), config)
+        end
+  
+        def logdev_from_config(config)
+          case c = config[:log]
+          when nil  
+            STDERR
+          when '-'
+            STDOUT
+          else
+            c
+          end
+        end
+      end
     end
-end
+  end
